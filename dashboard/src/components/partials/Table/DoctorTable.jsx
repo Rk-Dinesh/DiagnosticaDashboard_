62445bea1426907497e1f062e81f10f90db7521c
Rk-Dinesh <rdkumar881@gmail.com>
2024-01-05 11:23:28 +0530

    pagination

--- dashboard/src/components/partials/Table/DoctorTable.jsx
+++ dashboard/src/components/partials/Table/DoctorTable.jsx
@@ -185,6 +185,8 @@ const DoctorTable = ({Current_user}) => {
     
   };
 
+  const range = 1;
+
   return (
     <>
       <div className="md:flex justify-between items-center mb-6">
@@ -300,21 +302,30 @@ const DoctorTable = ({Current_user}) => {
               Prev
             </button>
           </li>
-          {pageOptions.map((page, pageIdx) => (
+          {pageOptions.map((page, pageIdx) => {
+        if (
+          pageIdx === pageIndex ||
+          (pageIdx >= pageIndex - range && pageIdx <= pageIndex + range)
+        ) {
+          return (
             <li key={pageIdx}>
               <button
                 href="#"
                 aria-current="page"
-                className={`${pageIdx === pageIndex
-                  ? "bg-slate-900 dark:bg-slate-600  dark:text-slate-200 text-white font-medium "
-                  : "bg-slate-100 dark:bg-slate-700 dark:text-slate-400 text-slate-900  font-normal  "
-                  } text-sm rounded leading-[16px] flex h-6 w-6 items-center justify-center transition-all duration-150`}
+                className={`${
+                  pageIdx === pageIndex
+                    ? "bg-slate-900 dark:bg-slate-600  dark:text-slate-200 text-white font-medium"
+                    : "bg-slate-100 dark:bg-slate-700 dark:text-slate-400 text-slate-900  font-normal"
+                } text-sm rounded leading-[16px] flex h-6 w-6 items-center justify-center transition-all duration-150`}
                 onClick={() => gotoPage(pageIdx)}
               >
                 {page + 1}
               </button>
             </li>
-          ))}
+          );
+        }
+        return null;
+      })}
           <li className="text-sm leading-4 text-slate-900 dark:text-white rtl:rotate-180">
             <button
               className={` ${!canNextPage ? "opacity-50 cursor-not-allowed" : ""
